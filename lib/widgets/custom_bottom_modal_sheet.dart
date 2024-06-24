79d45daf6dd8943b242d30b67098b7efab64c073
Hazem <hzm@gmail.com>
2024-06-24 21:42:24 +0300

    create bloc consumer of add note bottom sheet (integerate bloc step)

--- lib/widgets/custom_bottom_modal_sheet.dart
+++ lib/widgets/custom_bottom_modal_sheet.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_states.dart';
 import 'package:notes_app/widgets/add_note.dart';
 
 class AddBottomSheet extends StatelessWidget {
@@ -6,10 +10,22 @@ class AddBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
+    return SingleChildScrollView(
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        child: AddNote(),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: BlocConsumer<AddNoteCubit, AddNoteStates>(
+          listener: (context, state) {
+            if (state is FailureAddNoteState) {
+            } else if (state is SuccessAddNoteState) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+                inAsyncCall: (state is LoadingAddNoteState) ? true : false,
+                child: const AddNote());
+          },
+        ),
       ),
     );
   }
